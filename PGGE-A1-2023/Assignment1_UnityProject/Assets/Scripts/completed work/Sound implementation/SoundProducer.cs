3ec3f932dbaded627e1f1a387bba44530b30c94e
Thom Low <thomthomlow@gmail.com>
2023-11-20 18:05:41 +0800

    20 november 2023
    added custom editor to sound producer

--- PGGE-A1-2023/Assignment1_UnityProject/Assets/Scripts/completed work/Sound implementation/SoundProducer.cs
+++ PGGE-A1-2023/Assignment1_UnityProject/Assets/Scripts/completed work/Sound implementation/SoundProducer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -9,6 +10,22 @@ public class SoundProducer : MonoBehaviour
     private AudioSource audioSource;
     private Dictionary<string, Surface> keySurfaceValues = new Dictionary<string, Surface>();
 
+    #region walking clips and range of audio
+    //adjusting the min/ max volumn range
+    [SerializeField] private float minWalkVolumnRange = 0.1f;
+    [SerializeField]private float maxWalkVolumnRange = 1.0f;
+    
+    //adjusting the min/max pitch range
+    [SerializeField]private float minWalkPitchRange = 0.1f;
+    [SerializeField] private float maxWalkPitchRange = 1.0f;
+
+    #region grass walking
+    [SerializeField] private AudioClip[] walkingAudioClipGrass;
+    #endregion
+
+
+    #endregion
+
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -17,10 +34,11 @@ public class SoundProducer : MonoBehaviour
 
     private void PopulateValues()
     {
-        keySurfaceValues.Add("Grass pattern 01", Surface.grass);
-        keySurfaceValues.Add("wall", Surface.concrete);
-        keySurfaceValues.Add("Ground & rocks pattern 01", Surface.stone);
-        keySurfaceValues.Add("Ground & weeds", Surface.grassWeed);
+        //have to add (Instance) at the back.
+        keySurfaceValues.Add("Grass pattern 01 (Instance)", Surface.grass);
+        keySurfaceValues.Add("wall (Instance)", Surface.concrete);
+        keySurfaceValues.Add("Ground & rocks pattern 01 (Instance)", Surface.stone);
+        keySurfaceValues.Add("Ground & weeds (Instance)", Surface.grassWeed);
     }
     private enum Surface
     {
@@ -32,17 +50,22 @@ public class SoundProducer : MonoBehaviour
 
     private Surface GetsurfaceOfPlayer()
     {
-        Ray ray = new Ray(playerTransform.position, Vector3.down);
+        Vector3 offsetPosition = new Vector3(transform.position.x, 
+            transform.position.y + 1f , 
+            transform.position.z); //offset the height for raycasting 
+
+        Ray ray = new Ray(offsetPosition, Vector3.down);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 10f))
+        if(Physics.Raycast(ray, out hit))
         {
             
             if(hit.collider.TryGetComponent<MeshRenderer>(out MeshRenderer renderer))
             {
                 print(renderer.material.name);
-                if (keySurfaceValues.TryGetValue(renderer.material.name,out Surface surface))
+                if (keySurfaceValues.TryGetValue(renderer.material.name , out Surface surface))
                 {
                     print(surface);
+                    return surface;
                 }
             }
         }
@@ -51,7 +74,8 @@ public class SoundProducer : MonoBehaviour
 
     public void WalkingSoundPlay()
     {
-        GetsurfaceOfPlayer();
+        Surface surface= GetsurfaceOfPlayer();
+
     }
 
     public void RunningSoundPlay()
---
+++ PGGE-A1-2023/Assignment1_UnityProject/Assets/Scripts/completed work/Sound implementation/SoundProducerEditor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SoundProducer))]
+public class SoundProducerEditor : Editor
+{
+    private SerializedProperty transform;
+
+    #region walking clips
+    private SerializedProperty minWalkVolumnRange; //getting the values from the sound Producer
+    private SerializedProperty maxWalkVolumnRange;
+    private SerializedProperty minWalkPitchRange;
+    private SerializedProperty maxWalkPitchRange;
+    private SerializedProperty walkingAudioClipsGrass;
+
+    private float minWalkingVolumnRangeFloat = 0f;//to adjust the volumn of the walk
+    private float maxWalkingVolumnRangeFloat = 1f;
+
+    private float maxWalkingPitchRangeFloat = 1f; //to adjust the pitch of the walk
+    private float minWalkingPitchRangeFloat = 0f;
+
+    private bool walkingSoundFoldoutGroup = false; //used for the fold out group
+    #endregion 
+
+    private void OnEnable()
+    {
+        transform = serializedObject.FindProperty("playerTransform");
+        minWalkPitchRange = serializedObject.FindProperty("minWalkPitchRange");
+        maxWalkPitchRange = serializedObject.FindProperty("maxWalkPitchRange");
+        minWalkVolumnRange = serializedObject.FindProperty("minWalkVolumnRange");
+        maxWalkVolumnRange = serializedObject.FindProperty("maxWalkVolumnRange");
+        walkingAudioClipsGrass = serializedObject.FindProperty("walkingAudioClipGrass");
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(transform);
+
+        walkingSoundFoldoutGroup = EditorGUILayout.BeginFoldoutHeaderGroup(walkingSoundFoldoutGroup, "Walking sound");
+        if(walkingSoundFoldoutGroup)
+        {
+            EditorGUILayout.LabelField($"min pitch: {minWalkingPitchRangeFloat}");
+            EditorGUILayout.LabelField($"max pitch: {maxWalkingPitchRangeFloat}");
+            EditorGUILayout.MinMaxSlider("Range of Pitch",
+                ref minWalkingPitchRangeFloat,
+                ref maxWalkingPitchRangeFloat,
+                0f,
+                1f
+                );
+            EditorGUILayout.LabelField($"min volumn: {minWalkingVolumnRangeFloat}");
+            EditorGUILayout.LabelField($"max volumn: {maxWalkingVolumnRangeFloat}");
+            EditorGUILayout.MinMaxSlider("Range of Volumn",
+                ref minWalkingVolumnRangeFloat,
+                ref maxWalkingVolumnRangeFloat,
+                0f,
+                1f
+                );
+            minWalkPitchRange.floatValue = minWalkingPitchRangeFloat;
+            maxWalkPitchRange.floatValue= maxWalkingPitchRangeFloat;
+
+            minWalkVolumnRange.floatValue = minWalkingVolumnRangeFloat;
+            maxWalkVolumnRange.floatValue = maxWalkingVolumnRangeFloat;
+
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        EditorGUILayout.PropertyField(walkingAudioClipsGrass);
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
