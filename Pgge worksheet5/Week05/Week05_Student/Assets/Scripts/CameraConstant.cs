5e26c37d034e43ca2d94ab3c6dc085b47d2425a4
Thom Low <thomthomlow@gmail.com>
2024-02-06 10:27:21 +0800

    Nothing change

---
+++ Pgge worksheet5/Week05/Week05_Student/Assets/Scripts/CameraConstant.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraConstant
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
